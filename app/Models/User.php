f6c9aa723b6a0e021b9c1b8ec306f6b2a33a4aaa
romainchavalle <165096334+romainchavalle@users.noreply.github.com>
2026-03-29 22:12:53 +0200

    fix: add HasApiTokens and notes relationship to User model, add Postman collection, update dependencies

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -57,5 +58,13 @@ class User extends Authenticatable
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+    /**
+     * Obtenir les notes de l'utilisateur.
+     */
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
     }
 }
